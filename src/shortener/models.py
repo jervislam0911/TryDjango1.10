71e9c10e930ce4ffdd784128c26f88da90d3bc5c
Jervis <lin.jervis@yahoo.com>
2016-11-18 23:18:32 -0500

    adding KirrURL module

---
+++ src/shortener/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class KirrURL(models.Model):
+	"""docstring for KirrURL"""
+	url = models.CharField(max_length=220,)
+	shortcode = models.CharField(max_length=15, unique=True)
+
+	def __str__(self):
+		return str(self.url)
+		
