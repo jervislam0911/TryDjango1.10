f459d1add0fceb1d288793103dbef23e459d297c
Jervis <lin.jervis@yahoo.com>
2016-11-20 14:30:38 -0500

    add Command for refresh codes

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -7,12 +7,15 @@ class KirrURLManager(models.Manager):
 		qs_main = super(KirrURLManager, self).all(*args, *kwargs)
 		qs = qs_main.filter(active=True)
 		return qs
-	def refresh_shortcodes(self):
+	def refresh_shortcodes(self, items=None):
 		qs = KirrURL.objects.filter(id__gte=1)
+		if items is not None and isinstance(items, int):
+			qs = qs.order_by('-id')[:items]
 		new_codes = 0
 		for q in qs:
 			q.shortcode = create_shortcode(q)
 			print(q.shortcode)
+			print(q.id)
 			q.save()
 			new_codes +=1
 		return "New codes made: {new_code}".format(new_code=new_codes)
