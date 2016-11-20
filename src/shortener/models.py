4228ab583ff3cffad4745dec27c1509fac1a56ea
Jervis <lin.jervis@yahoo.com>
2016-11-20 01:13:27 -0500

    adding refresh code method to model manager

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from .utils import code_generator, create_shortcode
 
 # Create your models here.
+class KirrURLManager(models.Manager):
+	def all(self, *args, **kwargs):
+		qs_main = super(KirrURLManager, self).all(*args, *kwargs)
+		qs = qs_main.filter(active=True)
+		return qs
+	def refresh_shortcodes(self):
+		qs = KirrURL.objects.filter(id__gte=1)
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			print(q.shortcode)
+			q.save()
+			new_codes +=1
+		return "New codes made: {new_code}".format(new_code=new_codes)
+
+
 class KirrURL(models.Model):
 	"""docstring for KirrURL"""
 	url = models.CharField(max_length=220,)
@@ -10,6 +26,9 @@ class KirrURL(models.Model):
 	updated = models.DateTimeField(auto_now=True)
 	# everytime the model is created 
 	timestamp = models.DateTimeField(auto_now_add=True)
+	# active url
+	active  = models.BooleanField(default=True)
+	objects = KirrURLManager()
 
 	def save(self, *args, **kwargs):
 		if self.shortcode is None or self.shortcode == "":
--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -7,7 +7,7 @@ def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
 def create_shortcode(instance, size=6):
 	new_code = code_generator(size=size)
 	Klass = instance.__class__
-	qs = Klass.objects.filter(shortcode=new_code).exists()
+	qs_exists = Klass.objects.filter(shortcode=new_code).exists()
 	if qs_exists:
 		return create_shortcode(size=size)
 	return new_code
