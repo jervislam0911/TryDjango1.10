c2637261180bd3bed90d7158431e8ffbb08bc5e9
Jervis <lin.jervis@yahoo.com>
2016-11-27 22:40:02 -0500

    add Django Hosts and Custom Redirect URL

---
+++ src/krr/hostsconf/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseRedirect
+from django.conf import settings
+
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "http://www.tirr.com:8000")
+
+def wildcard_redirect(request, path=None):
+	new_url = DEFAULT_REDIRECT_URL
+	print("wa")
+	if path is not None:
+		new_url = DEFAULT_REDIRECT_URL + "/" + path
+		print('what?'+ new_url)
+	return HttpResponseRedirect(new_url)
