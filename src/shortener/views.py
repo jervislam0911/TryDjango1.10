fd57f2fdb833772d04b427cd802f15b141c461bc
Jervis <lin.jervis@yahoo.com>
2016-11-27 22:38:25 -0500

    error correction

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.http import HttpResponse
 from django.views import View
 from .models import KirrURL
 
 
-
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
-	obj = KirrURL.objects.get(shortcode=shortcode)
-	return HttpResponse('hello {sc}'.format(sc=obj.url))
+	obj = get_object_or_404(KirrURL, shortcode=shortcode)
+	print('hello {sc}'.format(sc=shortcode))
+	return HttpResponseRedirect(obj.url)
 
 
 class KirrCBView(View):
 	def get(self,request, shortcode=None, *args, **kwargs):
-		return HttpResponse('hello again {sc}'.format(sc=shortcode))
-		
+		obj = get_object_or_404(KirrURL, shortcode=shortcode)
+		return HttpResponseRedirect(obj.url)
+	
+	def post(self, request, *args, **kwargs):
+		return HttpResponse()
